1323194d8b03d90ac8fa75407911b71da03de2bf
Andrew Ramires May <andrewc3po@gmail.com>
2023-11-30 17:39:44 -0400

    Adicao de endereco e remocao de acentos

---
+++ Scripts/endereco.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE VIEW  TIPOENDERECO AS
+SELECT
+	tipo."AdresType" AS "IDTIPOENDERECOERP",
+	CASE
+		WHEN tipo."AdresType" = 'S' THEN 'ENTREGA'
+		WHEN tipo."AdresType" = 'B' THEN 'COBRANCA'
+	END AS "DESCRICAO",
+	tipo."AdresType" AS "TIPO"
+FROM
+	CRD1 AS tipo
+GROUP BY tipo."AdresType";
+	
+
+CREATE OR REPLACE VIEW  ENDERECO AS
+SELECT 
+	"CardCode"||'-'||"AdresType"||'-'||"Address"  AS "IDENDERECOERP",
+	"AdresType" AS "IDTIPOENDERECOERP",
+	"CardCode" AS "IDCLIENTEERP",
+	"County" AS "IDCIDADEERP",
+	'' AS "NOME",
+	'' AS "RSOCIAL",
+	fncRemove_Acentuacao("Street").resultado AS "ENDERECO",
+	SUBSTRING("StreetNo",0,15)  AS "NUMERO",
+	"Block" AS "BAIRRO",
+	fncRemove_Acentuacao("Building").resultado AS "COMPLEMENTO",
+	SUBSTRING("ZipCode",0,10)  AS "CEP",
+	"U_TX_IE" AS "IERG",
+	'' AS "CNPJCPF",
+	'' AS "TELEFONE",
+	'' AS "FAX",
+	'' AS "CELULAR",
+	'' AS "LATITUDE",
+	'' AS "LONGITUDE",
+	'' AS "PRECISAO"
+FROM CRD1
+INNER JOIN cliente ON cliente.IDCLIENTEERP = CRD1."CardCode" ;
---
+++ Scripts/removeAcento.sql
@@ -0,0 +1,27 @@
+
+
+
+CREATE OR REPLACE FUNCTION fncRemove_Acentuacao(val VARCHAR(255))
+RETURNS resultado VARCHAR(255) LANGUAGE SQLSCRIPT AS
+BEGIN
+	resultado := LOWER(val);
+	resultado := REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(resultado,'á','a'),'à','a'),'â','a'),'ã','a'),'ä','a');
+	resultado := REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(resultado,'á','a'),'à','a'),'â','a'),'ã','a'),'ä','a');
+    resultado :=  REPLACE(REPLACE(REPLACE(REPLACE(resultado,'é','e'),'è','e'),'ê','e'),'ë','e');
+    resultado :=  REPLACE(REPLACE(REPLACE(REPLACE(resultado,'í','i'),'ì','i'),'î','i'),'ï','i');
+    resultado :=  REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(resultado,'ó','o'),'ò','o'),'ô','o'),'õ','o'),'ö','o');
+    resultado :=  REPLACE(REPLACE(REPLACE(REPLACE(resultado,'ú','u'),'ù','u'),'û','u'),'ü','u');
+
+   	resultado := REPLACE(resultado,'ý','y');
+    resultado := REPLACE(resultado,'ñ','n');
+    resultado := REPLACE(resultado,'ç','c');
+   
+   resultado := REPLACE(resultado,',','');
+  
+   resultado := REPLACE(resultado,'.','');
+
+END;
+
+
+
+
