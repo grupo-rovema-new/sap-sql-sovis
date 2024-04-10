aab0b1e26bc60c7f3ca8e0af605458d8285eb481
Andrew Ramires May <andrewc3po@gmail.com>
2024-04-10 16:54:56 -0400

    adiciona coluna

--- src/views/util/BpCpfCnpj.sql
+++ src/views/util/BpCpfCnpj.sql
@@ -4,7 +4,8 @@ CREATE OR REPLACE VIEW BpCpfCnpj AS
 		CASE 
 			WHEN "TaxId4" = '' OR "TaxId4" IS null THEN "TaxId0"
 			ELSE "TaxId4"
-		END AS cpfCnpj
+		END AS cpfCnpj,
+		'windson' AS  windson
 	FROM CRD7 WHERE (("TaxId4" IS NOT NULL AND "TaxId4" <> '' AND ("TaxId0" IS NULL OR "TaxId0" = '')) or ("TaxId0" IS NOT null AND "TaxId0" <> '' AND ("TaxId4" IS NULL OR "TaxId4" = '')))
 
 
