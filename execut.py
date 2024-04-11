7e98c7b12c40a569e76520fc3f4467f6533393ba
Andrew Ramires May <andrewc3po@gmail.com>
2024-04-11 16:54:30 -0400

    Adicao de log

--- execut.py
+++ execut.py
@@ -18,6 +18,7 @@ cursor = conn.cursor()
 for filename in glob.iglob('src/views/util/**/*.sql', recursive = True): 
     with open(filename, 'r') as file:
         data = file.read().replace('\n', '')
+        print(filename)
         cursor.execute(data)
 
 cursor.close()
