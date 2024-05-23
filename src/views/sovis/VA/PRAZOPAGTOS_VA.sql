4db739a9e93f2a11f82b2fff63d8626248bfb90d
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-05-23 17:10:00 -0400

    Criação de views para o SOVIS VA (#17)

---
+++ src/views/sovis/VA/PRAZOPAGTOS_VA.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE VIEW PRAZOPAGTOS_VA AS 
+((SELECT
+		"Code" || '_' || "U_prazo" AS "IDPRAZOPAGTOERP",
+		t."ListName" ||' | ' ||"PymntGroup" AS "DESCRICAO",
+		"LatePyChrg" AS "JURO",
+		"InstNum" AS "QTDEPARCELAS",
+		NULL AS "TIPO"
+	FROM
+		"@CONDICOESFV" cond
+		INNER JOIN OCTG ON cond."U_prazo" = OCTG."GroupNum" 
+		INNER JOIN OPLN t on(t."U_tipoComissao" = cond."Code")
+	WHERE
+		t."U_tipoComissao" IS NOT null) UNION ALL (SELECT
+		CAST(OCTG."GroupNum" AS varchar)  AS "IDPRAZOPAGTOERP",
+		"PymntGroup" AS "DESCRICAO","LatePyChrg" AS "JURO",
+		"InstNum" AS "QTDEPARCELAS",
+		NULL AS "TIPO"
+	FROM
+		OCTG));
