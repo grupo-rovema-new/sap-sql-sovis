4c61cd78791fdf3d3fefc594736741ef8895a0ab
Andrew Ramires <8061215+malacration@users.noreply.github.com>
2024-06-03 17:03:06 -0400

    Adição de campos para Sovis VA (#19)
    * wip
    
    * correcao

--- src/views/sovis/VA/ASSTABPRECOPRODUTO_VA.sql
+++ src/views/sovis/VA/ASSTABPRECOPRODUTO_VA.sql
@@ -1,18 +1,12 @@
   CREATE OR REPLACE VIEW ASSTABPRECOPRODUTO_VA AS
 SELECT
-		ITM1."PriceList" || ' - ' || ITM1."ItemCode" AS "IDASSTABPRECOPRODUTOERP",
-		ITM1."PriceList"  AS "IDTABPRECOERP",
-		ITM1."ItemCode"  AS "IDPRODUTOERP",
-		"Price" AS "VALOR",
-		"Price"-((c."U_desconto"/100)*"Price") AS "VALORMINIMO", --formula que calcula o valor minino de desconto, usando o campo U_desconto
-		c."U_desconto" AS "DESCONTOMAXIMO"
+		"IDTABPRECOERP" || ' - ' || "IDPRODUTOERP" AS "IDASSTABPRECOPRODUTOERP",
+		"IDTABPRECOERP"  AS "IDTABPRECOERP",
+		"IDPRODUTOERP"  AS "IDPRODUTOERP",
+		"VALOR1" AS "VALOR",
+		"VALORMAXIMO" AS "VALORMINIMO",
+		"VALORMINIMO" AS "VALORMAXIMO",
+		"DESCMAXIMO1" AS "DESCONTOMAXIMO"
 	FROM
-		ITM1
-		LEFT JOIN OPLN p on(p."ListNum" = ITM1."PriceList")
-		LEFT JOIN "@COMISSAO" c on(c."Code" = p."U_tipoComissao")
-	WHERE
-		"PriceList" IN (SELECT IDTABPRECOERP FROM TABPRECO t)
-		AND "ItemCode" in(SELECT IDPRODUTOERP FROM produto)
-		AND "Price" > 0
-		AND "ItemCode"  IN (SELECT T0."ItemCode" FROM OITM T0 WHERE T0."validFor"  = 'Y');  
+		"ASSTABPRECOPRODUTO"
   
