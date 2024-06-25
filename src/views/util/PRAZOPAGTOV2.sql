28665d57602cbc4341593df4936206c28cabb629
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-06-25 08:43:03 -0400

    Alteração da Descricao da view PRAZOPAGTO
    Foi alterado a ordem do campo DESCRICAO da view PRAZOPAGTO, de  t."ListName" ||' | ' ||"PymntGroup" AS "DESCRICAO", para "PymntGroup" ||' | ' || t."ListName" AS "DESCRICAO",

---
+++ src/views/util/PRAZOPAGTOV2.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW PRAZOPAGTO AS
+((SELECT
+		"Code" || '_' || "U_prazo" AS "IDPRAZOPAGTOERP",
+		 "PymntGroup" ||' | ' || t."ListName" AS "DESCRICAO",
+		1 AS "SITUACAO"
+	FROM
+		"@CONDICOESFV" cond
+		INNER JOIN OCTG ON cond."U_prazo" = OCTG."GroupNum" 
+		INNER JOIN OPLN t on(t."U_tipoComissao" = cond."Code")
+	WHERE
+		t."U_tipoComissao" IS NOT null) UNION ALL (SELECT
+		CAST(OCTG."GroupNum" AS varchar)  AS "IDPRAZOPAGTOERP",
+		"PymntGroup" AS "DESCRICAO",
+		1 AS "SITUACAO"
+	FROM
+		OCTG));
