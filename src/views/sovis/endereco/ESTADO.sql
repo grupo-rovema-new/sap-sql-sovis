21869a787ec5d3ee73c2aeea40baa7744546d627
Andrew Ramires May <andrewc3po@gmail.com>
2024-08-06 15:12:05 -0400

    correcao de bug

--- src/views/sovis/endereco/ESTADO.sql
+++ src/views/sovis/endereco/ESTADO.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE VIEW ESTADO AS
 SELECT
 	T0."Code" AS "IDESTADOERP",
-	T0."Name" AS "DESCRICAO"
+	T0."Code" AS "DESCRICAO" -- O sovis so aceita 2 caracteres na descricao do estado!
 FROM
 	OCST T0 
 	WHERE LENGTH(T0."Code") <= 2 AND T0."Country" = 'BR';
