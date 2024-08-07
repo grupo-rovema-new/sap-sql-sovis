68722349b6e53cd1d4afb36b0bc6819fa9af5e04
Andrew Ramires <8061215+malacration@users.noreply.github.com>
2024-08-07 14:14:52 -0400

    Adicao de transaction notification da Katrid (#40)
    * Adiciona view 2
    
    * Correcao em estado
    
    * limitar caracteres campo endereço (#30)
    
    Foi limitado o tamanho do campo endereço da view cliente, pois ultrapassava a quantidade de 50 caracteres
    
    * Criacao View (#31)
    
    Criação da view para uso no Power BI
    
    * Criação e desmembramento de views (#32)
    
    * Criacao View
    
    Criação da view para uso no Power BI
    
    * Desmembramento da view ASSCLIENTEUSUARIO
    
    Para resolver o problema da publicação da versão v2.4.0
    
    * Ajuste Trava (#34)
    
    Trava de cancelamento permitindo outros status que não deveriam passar.
    
    * Limitar Caracteres (#36)
    
    Foi adicionado a função SUBSTRING, nos campos OBSPEDIDO E OBSNOTA, com o objetivo de limitar os caracteres para 255, pois alguns dados estavam ultrapassando essa quantidade e gerando log de erro no sovis
    
    * Ajuste TituloAnotação (#37)
    
    Foi ajustado a view para que ela siga os mesmo critérios da view TITULO
    
    * Adicionando quey para bloqueio por filial (#33)
    
    * Teste da view
    
    * Metas
    
    * Mudando codigo para criar views da pasta comercial
    
    * Comissão
    
    * Com relatório de comissão
    
    * ajustando comissão
    
    * Arrumando erro do docentry
    
    * Retirando datas dos parametros
    
    * Ajustando relatório de comissão
    
    * Adicionando transaction de bloqueio por filial
    
    * Ajustando trava por utilização para resolver o problema das duas linhas. (#38)
    
    * Teste da view
    
    * Metas
    
    * Mudando codigo para criar views da pasta comercial
    
    * Comissão
    
    * Com relatório de comissão
    
    * ajustando comissão
    
    * Arrumando erro do docentry
    
    * Retirando datas dos parametros
    
    * Ajustando relatório de comissão
    
    * Adicionando transaction de bloqueio por filial
    
    * Aplicando trava de utilização
    
    * Ajustando para resolver o problema de mais linhas
    
    * Ajuste para a trava deixar passar notas que não sao de itens
    
    * Alteração view titulo (#39)
    
    Alterado a condição do DocStatus de O, para O e D
    
    * Resolucao de conflito
    
    * correcao de bug
    
    * Adicao de transaction da Katrid
    
    * Correcao de conflito
    
    ---------
    
    Co-authored-by: rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
    Co-authored-by: alefpinheiro <129449313+alefpinheiro@users.noreply.github.com>
    Co-authored-by: Francisco Marcello <54758350+franciscoMarcello@users.noreply.github.com>

--- src/views/sovis/endereco/ESTADO.sql
+++ src/views/sovis/endereco/ESTADO.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW ESTADO AS
 SELECT
-	T0."Code" AS "IDESTADOERP",
+	T0."Code" AS "DESCRICAO" -- O sovis so aceita 2 caracteres na descricao do estado!
 	T0."Name" AS "DESCRICAO"
 FROM
 	OCST T0 
