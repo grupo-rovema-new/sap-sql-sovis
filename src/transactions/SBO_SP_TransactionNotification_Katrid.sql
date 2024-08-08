28aeec27aae39c4412fe85de815f467588aedabb
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-08-08 15:40:05 -0400

    Update SBO_SP_TransactionNotification_Katrid.sql (#43)
    * Update SBO_SP_TransactionNotification_Katrid.sql
    
    Alteração procedure Katrid
    
    * Update SBO_SP_TransactionNotification_Katrid.sql
    
    alteração script

--- src/transactions/SBO_SP_TransactionNotification_Katrid.sql
+++ src/transactions/SBO_SP_TransactionNotification_Katrid.sql
@@ -12,8 +12,9 @@ LANGUAGE SQLSCRIPT
 SQL SECURITY INVOKER
 AS
 begin
+IF (:transaction_type = 'A' or :transaction_type = 'U') THEN
 	-- PARCEIRO DE NEGOCIO
-	IF ((:object_type = '2') and (:transaction_type = 'A' or :transaction_type = 'U')) THEN
+	IF (:object_type = '2') THEN
 		INSERT INTO
 			"@KATRID_INTE" (
 				"U_Object_Name",
@@ -38,7 +39,7 @@ begin
 			);
 
 	-- ITEM
-	ELSEIF ((:object_type='4') AND (:transaction_type = 'A' or :transaction_type = 'U')) THEN
+	ELSEIF (:object_type = '4') THEN
 	INSERT INTO
 			"@KATRID_INTE" (
 				"U_Object_Name",
@@ -63,7 +64,7 @@ begin
 			);
 
 	-- UTILIZAÇÕES
-	ELSEIF ((:object_type='260') AND (:transaction_type = 'A' or :transaction_type = 'U')) THEN
+	ELSEIF (:object_type = '260') THEN
 	INSERT INTO
 			"@KATRID_INTE" (
 				"U_Object_Name",
@@ -86,5 +87,131 @@ begin
 				list_of_cols_val_tab_del,
 				transaction_type
 			);
+	
+	-- NF DE SAIDA
+	ELSEIF((:object_type = '13') ) THEN
+		INSERT INTO
+			"@KATRID_INTE" (
+				"U_Object_Name",
+				"DocEntry",
+				"Object",
+				"RequestStatus"
+			)
+		values
+			(
+				'Invoices',
+				coalesce(
+					(
+						select
+							max("DocEntry") + 1
+						from
+							"@KATRID_INTE"
+					),
+					1
+				),
+				list_of_cols_val_tab_del,
+				transaction_type
+			);
+	
+	-- NF DE ENTRADA
+	ELSEIF((:object_type = '18') ) THEN
+		INSERT INTO
+			"@KATRID_INTE" (
+				"U_Object_Name",
+				"DocEntry",
+				"Object",
+				"RequestStatus"
+			)
+		values
+			(
+				'PurchaseInvoices',
+				coalesce(
+					(
+						select
+							max("DocEntry") + 1
+						from
+							"@KATRID_INTE"
+					),
+					1
+				),
+				list_of_cols_val_tab_del,
+				transaction_type
+			);
+		
+	-- DEV DE SAIDA
+	ELSEIF((:object_type = '14') ) THEN
+		INSERT INTO
+			"@KATRID_INTE" (
+				"U_Object_Name",
+				"DocEntry",
+				"Object",
+				"RequestStatus"
+			)
+		values
+			(
+				'CreditNotes',
+				coalesce(
+					(
+						select
+							max("DocEntry") + 1
+						from
+							"@KATRID_INTE"
+					),
+					1
+				),
+				list_of_cols_val_tab_del,
+				transaction_type
+			);
+	
+	-- DEV DE ENTRADA
+	ELSEIF((object_type = '19') THEN
+		INSERT INTO
+			"@KATRID_INTE" (
+				"U_Object_Name",
+				"DocEntry",
+				"Object",
+				"RequestStatus"
+			)
+		values
+			(
+				'PurchaseCreditNotes',
+				coalesce(
+					(
+						select
+							max("DocEntry") + 1
+						from
+							"@KATRID_INTE"
+					),
+					1
+				),
+				list_of_cols_val_tab_del,
+				transaction_type
+			);
+	
+	-- ENTREGA
+	ELSEIF(:object_type = '15') THEN
+		INSERT INTO
+			"@KATRID_INTE" (
+				"U_Object_Name",
+				"DocEntry",
+				"Object",
+				"RequestStatus"
+			)
+		values
+			(
+				'DeliveryNotes',
+				coalesce(
+					(
+						select
+							max("DocEntry") + 1
+						from
+							"@KATRID_INTE"
+					),
+					1
+				),
+				list_of_cols_val_tab_del,
+				transaction_type
+			);
 	END IF;
+END IF;
 end;
