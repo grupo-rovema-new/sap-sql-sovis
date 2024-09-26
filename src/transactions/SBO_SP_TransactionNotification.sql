de57179a5720d0f1cd55c8d6c2676ff7f46f7a99
Francisco Marcello <fmarcello265@gmail.com>
2024-09-26 14:27:40 -0400

    ajustando para adicionar a linha que chama as travas da katrid

--- src/transactions/SBO_SP_TransactionNotification.sql
+++ src/transactions/SBO_SP_TransactionNotification.sql
@@ -246,6 +246,7 @@ Call SBO_SP_TRANSACTIONNOTIFICATION_Liberali(object_type,transaction_type,num_of
 Call SBO_SP_TRANSACTIONNOTIFICATION_ROVEMA(object_type,transaction_type,num_of_cols_in_key,list_of_key_cols_tab_del,list_of_cols_val_tab_del,error,error_message);
 Call SBO_SP_Validacao_Bloqueio_Periodo_Contabil(object_type,transaction_type,num_of_cols_in_key,list_of_key_cols_tab_del,list_of_cols_val_tab_del,error,error_message);
 Call SBO_SP_VALIDACAO_POR_UTILIZACAO(object_type,transaction_type,num_of_cols_in_key,list_of_key_cols_tab_del,list_of_cols_val_tab_del,error,error_message);
+Call SBO_SP_TransactionNotification_Katrid(object_type,transaction_type,num_of_cols_in_key,list_of_key_cols_tab_del,list_of_cols_val_tab_del,error,error_message);
 Call SBO_SP_VALIDACAO_VENDA_FUTURA(object_type,transaction_type,num_of_cols_in_key,list_of_key_cols_tab_del,list_of_cols_val_tab_del,error,error_message);
 
 -- LANÇAMENTO CONTABIL MANUAL
@@ -279,9 +280,9 @@ IF :object_type = '59' and (:transaction_type = 'A' OR :transaction_type = 'U') 
 
 		END IF;
 END IF;
-/*
+
 -- NOTA FISCAL DE SAÍDA
-IF :object_type = '13' and (:transaction_type = 'A' OR :transaction_type = 'U') then 
+/*IF :object_type = '13' and (:transaction_type = 'A' OR :transaction_type = 'U') then 
 	
 	Select 
 		count(1) 
@@ -313,8 +314,8 @@ IF :object_type = '13' and (:transaction_type = 'A' OR :transaction_type = 'U') 
          		error_message := XITEM;  
 
 		END IF;
-END IF;
-*/
+END IF;*/
+
 
 select :error, SUBSTRING (:error_message,0,255) AS error_message FROM dummy;
 
