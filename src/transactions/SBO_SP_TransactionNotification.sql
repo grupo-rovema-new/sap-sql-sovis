f50def40d58f7e9a784713114738cf7a79440262
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-10-07 10:56:33 -0400

    Trans funcion trava (#68)
    * Transaction e Function Trava
    
    Trava do pedido, quantidade maior que o pedido
    
    * Update SBO_SP_TransactionNotification.sql
    
    correção da sintaxe

--- src/transactions/SBO_SP_TransactionNotification.sql
+++ src/transactions/SBO_SP_TransactionNotification.sql
@@ -282,9 +282,6 @@ IF :object_type = '59' and (:transaction_type = 'A' OR :transaction_type = 'U') 
 		END IF;
 END IF;
 
-select :error, SUBSTRING (:error_message,0,255) AS error_message FROM dummy;
-
-end;
 -- NOTA FISCAL DE SAÍDA
 IF :object_type = '13' and (:transaction_type = 'A' OR :transaction_type = 'U') then 
     
@@ -319,3 +316,7 @@ IF :object_type = '13' and (:transaction_type = 'A' OR :transaction_type = 'U') 
 
         END IF;
 END IF;
+
+select :error, SUBSTRING (:error_message,0,255) AS error_message FROM dummy;
+
+end;
