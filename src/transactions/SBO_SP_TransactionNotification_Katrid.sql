73e95bdbd22fc72b2878107c62dcc21b64997f85
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-11-22 15:43:15 -0400

    Modulo Colaboradores
    Adição do modulo colaboradores, na procedure TransactionNotification_Katrid.

--- src/transactions/SBO_SP_TransactionNotification_Katrid.sql
+++ src/transactions/SBO_SP_TransactionNotification_Katrid.sql
@@ -228,7 +228,33 @@ BEGIN
                 '67'
             FROM WTR1
             WHERE "DocEntry" = list_of_cols_val_tab_del;
-
+           
+		-- COLABORADORES
+        ELSEIF (object_type = '171') THEN
+            INSERT INTO
+                "@KATRID_INTE" (
+                    "U_Object_Name",
+                    "DocEntry",
+                    "Object",
+                    "RequestStatus",
+                    "DocNum"
+                )
+            VALUES
+                (
+                    'EmployeesInfo',
+                    COALESCE(
+                        (
+                            SELECT
+                                MAX("DocEntry") + 1
+                            FROM
+                                "@KATRID_INTE"
+                        ),
+                        1
+                    ),
+                    list_of_cols_val_tab_del,
+                    transaction_type,
+                    '171'
+                );
         END IF; 
     END IF;
 END;
