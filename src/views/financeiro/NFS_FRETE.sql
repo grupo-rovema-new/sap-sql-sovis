22704bcd496c96aad8e9b64161e802df6722c8f6
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-11-29 14:57:51 -0400

    Frete nf (#87)
    * View Frete NF
    
    View para uso no relatorio comissões representante
    
    * adição numpag
    
    adição do campo numpag na view
    
    * atualização das formulas
    
    atualização das formulas de frete e valor pago

--- src/views/financeiro/NFS_FRETE.sql
+++ src/views/financeiro/NFS_FRETE.sql
@@ -4,11 +4,16 @@ SELECT
 	T0."DocNum" AS "DocNum",
 	T2."DocNum" AS "N.Pag",
 	T5."BaseLine",
+	COALESCE(T8."LineTotal",0) AS "Frete", 
+	T0."DocTotal",
+	FA."faturado",
+	VP."Total pago",
+	t5."LineTotal" AS "TotalItem",
 	COALESCE(T12."LineTotal",0) AS "Frete",
-	(VP."Total pago" / NULLIF(T0."DocTotal",0)) * 100 AS "Percentual",
-	((VP."Total pago" / NULLIF(T0."DocTotal",0)) * 100)/ 100 AS "PorcFrete",
-	ROUND((((VP."Total pago" / NULLIF(T0."DocTotal",0)) * 100)/ 100)* COALESCE(T12."LineTotal",0),3) AS "VlrFrete",
-	ROUND(VP."Total pago"-(((VP."Total pago" / NULLIF(T0."DocTotal", 0)) * 100)/ 100)* COALESCE(T12."LineTotal", 0), 2) AS "PagSemFrete"
+	VP."Total pago"-COALESCE(T12."LineTotal",0) AS "PagoSemFrete",
+	(round((t5."LineTotal" / NULLIF(NV."TotalBruto",0)),2))*100 AS "Percentual",
+	round(((t5."LineTotal" / NULLIF(NV."TotalBruto",0)) * 100)*(T0."DiscPrcnt"/100),2) AS "%DescLinha",
+	(ROUND((t5."LineTotal" / NULLIF(NV."TotalBruto",0)),2)*VP."Total pago")-COALESCE(T12."LineTotal",0) AS "TotPgSFrete"
 FROM
 	OINV T0
 INNER JOIN "INV6" T3 ON
@@ -23,6 +28,8 @@ INNER JOIN "ORCT" T2 ON
 	AND T2."Canceled" = 'N'
 INNER JOIN "INV1" T5 ON
 		T0."DocEntry" = T5."DocEntry"
+LEFT JOIN "INV3" T8 ON
+		T0."DocEntry" = T8."DocEntry"
 LEFT JOIN INV13 T12 ON
 		T5."DocEntry" = T12."DocEntry"
 	AND T5."LineNum" = T12."LineNum"
@@ -30,7 +37,14 @@ LEFT JOIN CR_VALORPAGO VP ON
 		T0."DocEntry" = VP."EntryNota"
 	AND T2."DocNum" = VP."N.Pag"
 	AND T2."DocEntry" = VP."EntryPag"
+INNER JOIN "NFS_FATURADO" FA ON
+		T0."DocEntry" = FA."EntryNota"
+	AND T0."DocNum" = FA."DocNum"
+	AND T5."BaseLine" = FA."BaseLine"
+LEFT JOIN NFS_VALORBRUTO NV ON
+		T0."DocEntry" = NV."DocEntry"
 WHERE 
 		T0."CANCELED" = 'N'
 	AND T0."DocDate" >= TO_DATE(20230701, 'YYYYMMDD')
-	AND T0."U_Rov_Refaturamento" = 'NAO';
+	AND T0."U_Rov_Refaturamento" = 'NAO'
+	ORDER by t0."DocEntry" ;
