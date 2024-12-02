20a1ef634bedabf6e6cd718731419328558567ee
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-12-02 09:37:57 -0400

    views rel comissões
    views para uso do relatorio de comissões

---
+++ src/views/financeiro/CR_TITULOSDESDOBRADO.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW SBOGRUPOROVEMA.CR_TITULOSDESDOBRADO AS
+SELECT
+	T0."DocEntry",
+	T0."DocNum",
+	COUNT(T2."DocNum") AS "NumDesdobr"
+FROM
+		"OINV" T0
+INNER JOIN "INV6" T3 ON
+		T3."DocEntry" = T0."DocEntry"
+INNER JOIN "RCT2" T1 ON
+		T1."DocEntry" = T0."DocEntry"
+	AND T1."DocTransId" = T0."TransId"
+	AND T1."InstId" = T3."InstlmntID"
+INNER JOIN "ORCT" T2 ON
+		T2."DocEntry" = T1."DocNum"
+	AND T2."Canceled" = 'N'
+WHERE 
+GROUP BY T0."DocEntry",T0."DocNum";
