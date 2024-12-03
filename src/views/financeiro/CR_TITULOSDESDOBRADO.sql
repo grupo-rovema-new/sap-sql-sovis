e3913f0e457f1a9e134ae644e735f7c7464c0d34
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2024-12-03 10:17:05 -0400

    UNION'S REL COMISSÃO (#93)
    Foi adicionado um union da tabela odpi de adiantamento de cliente, para que ela apareça no relatorio

--- src/views/financeiro/CR_TITULOSDESDOBRADO.sql
+++ src/views/financeiro/CR_TITULOSDESDOBRADO.sql
@@ -14,4 +14,23 @@ INNER JOIN "RCT2" T1 ON
 INNER JOIN "ORCT" T2 ON
 		T2."DocEntry" = T1."DocNum"
 	AND T2."Canceled" = 'N'
-GROUP BY T0."DocEntry",T0."DocNum";
+GROUP BY T0."DocEntry",T0."DocNum"
+
+UNION 
+
+SELECT --Fatura de Adiantamento de clientes
+	T0."DocEntry",
+	T0."DocNum",
+	COUNT(T2."DocNum") AS "NumDesdobr"
+FROM
+		"ODPI" T0
+INNER JOIN "DPI6" T3 ON
+		T3."DocEntry" = T0."DocEntry"
+INNER JOIN "RCT2" T1 ON
+		T1."DocEntry" = T0."DocEntry"
+	AND T1."DocTransId" = T0."TransId"
+	AND T1."InstId" = T3."InstlmntID"
+INNER JOIN "ORCT" T2 ON
+		T2."DocEntry" = T1."DocNum"
+	AND T2."Canceled" = 'N'
+GROUP BY T0."DocEntry",T0."DocNum"
