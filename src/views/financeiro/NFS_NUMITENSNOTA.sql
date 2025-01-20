bbb27ee1f4971a8ccf697cd6c2340161351b2836
rodrigocesar87 <99264107+rodrigocesar87@users.noreply.github.com>
2025-01-20 14:49:26 -0400

    Nfsfrete2025 (#119)
    * Novos views para frete
    
    Foi adicionado NFS_FRETELINHA e NFS_NUMITEMNOTA e ajustado views
    NFS_FRETE para uso no relatorio de comissões
    
    * Update NFS_NUMITENSNOTA.sql
    
    Ajuste no replace da view
    
    * Ajuste NFS_FRETE
    
    Foi ajustado o campo percentual, pois estava arredondando o valor final e gerando erro no valor pago

--- src/views/financeiro/NFS_NUMITENSNOTA.sql
+++ src/views/financeiro/NFS_NUMITENSNOTA.sql
@@ -1,2 +1,8 @@
 CREATE OR REPLACE VIEW NFS_NUMITENSNOTA AS
-SELECT I."DocEntry",count(I."DocEntry") AS "N.Itens" FROM inv1 i GROUP BY I."DocEntry";
+SELECT
+	I."DocEntry",
+	count(I."DocEntry") AS "N.Itens"
+FROM
+	inv1 i
+GROUP BY
+	I."DocEntry";
