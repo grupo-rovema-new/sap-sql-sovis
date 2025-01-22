04371390cf76d9bf03c7ac4dff50802717c9bb33
Francisco Marcello <54758350+franciscoMarcello@users.noreply.github.com>
2025-01-22 14:02:58 -0400

    Ajustando VIEW resumo avulso (#124)
    * Teste da view
    
    * Metas
    
    * Mudando codigo para criar views da pasta comercial
    
    * Comissão
    
    * Com relatório de comissão
    
    * ajustando comissão
    
    * Arrumando erro do docentry
    
    * Retirando datas dos parametros
    
    * Ajustando relatório de comissão
    
    * Adicionando transaction de bloqueio por filial
    
    * Aplicando trava de utilização
    
    * Ajustando para resolver o problema de mais linhas
    
    * Ajuste para a trava deixar passar notas que não sao de itens
    
    * Correcao endereco
    
    * Funcionalidade: validação da venda futura
    
    * Comentando trava para validacao de venda futura
    
    * Liberacao das travas para permitir venda futura
    
    * Adicionando trava para não permitir quantidade acima que a do contrato
    
    * Removendo comentarios
    
    * Ajustando trava de pis e cofins
    
    * ajustando branch
    
    * Adicionando travas de CTE
    
    * Adicionando trava para utilizacao de doacao e brinde
    
    * Adicionando utilizacao de bonificacao na trava de destinacao
    
    * Comentando trava do CTE e adicionando View para o relatorio de pis e cofins
    
    * Comentando trava
    
    * Ajustando VIEW resumo avulso
    
    ---------
    
    Co-authored-by: Andrew Ramires May <andrewc3po@gmail.com>

--- src/views/contabil/RESUMO_AVULSO_RESULTADO_PIS_COFINS.sql
+++ src/views/contabil/RESUMO_AVULSO_RESULTADO_PIS_COFINS.sql
@@ -1,3 +1,4 @@
+CREATE OR REPLACE VIEW RESUMO_AVULSO_RESULTADO_PIS_COFINS AS
 SELECT 
 L."BPLId",
 L."BPLName",
@@ -13,9 +14,6 @@ INNER JOIN OJDT LC  ON L."TransId" = LC."TransId"
 INNER JOIN OACT C ON L."Account" = C."AcctCode" 
 LEFT JOIN  OPRC CC ON L."OcrCode2" = CC."PrcCode" 
 WHERE "Account"  IN ('3.1.3.001.00002','3.1.3.001.00003','3.1.3.001.00004','3.1.3.001.00008','3.1.3.001.00010')
-AND L."RefDate" >= :Data
-AND L."RefDate" <= :Data1
-AND L."BPLId" = 2
 GROUP BY 
 C."AcctCode" || ' - ' || C."AcctName",
 CC."PrcCode" || ' - ' || CC."PrcName",
