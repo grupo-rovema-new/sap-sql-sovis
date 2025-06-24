a0788999f948b7b9b121d30f2968d9f64889d25d
Francisco Marcello <fmarcello265@gmail.com>
2025-06-24 17:06:18 -0400

    Adicionando trava de quantidade base da receita e processo de encerramento da ordem de producao

--- src/transactions/SBO_SP_VALIDACAO_PRODUCAO.sql
+++ src/transactions/SBO_SP_VALIDACAO_PRODUCAO.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE  PROCEDURE SBO_SP_VALIDACAO_PRODUCAO
+CREATE OR replace PROCEDURE SBO_SP_VALIDACAO_PRODUCAO
 
 (
     in object_type nvarchar(30),                 -- SBO Object Type
@@ -14,21 +14,17 @@ SQL SECURITY INVOKER
 AS
 BEGIN
 	
-	
-	
 IF :object_type IN('202') THEN
-
 	DECLARE idade INT;
     DECLARE temEntrada INT;
 	DECLARE temSaida INT;
-
-	DECLARE docNumMsg INT;
-	   
+	DECLARE ItemCode nvarchar (255);
+	DECLARE docNumMsg INT;  
 /*
  * Anotações
  * Status (L ->  Encerrado, R ->  Liberado, P -> Planejado)
  */
-
+	
 	SELECT DISTINCT 
 		DAYS_BETWEEN("CreateDate", CURRENT_DATE),
 		entrada."DocEntry" AS "temEntrada?",
@@ -39,7 +35,7 @@ IF :object_type IN('202') THEN
 		LEFT JOIN "IGN1" entrada ON(entrada."BaseRef" = to_char(ordem."DocNum"))
 		LEFT JOIN "IGE1" saida ON(saida."BaseRef" = to_char(ordem."DocNum"))
 	WHERE
-		"CreateDate" >= '2025-01-01'
+		"CreateDate" >= '2025-06-01'
 		AND ordem."DocEntry" = :list_of_cols_val_tab_del LIMIT 1;
 
 	-- Bloqueia modificar novos se existir ordem velhas planejada ou liberadas
@@ -50,7 +46,7 @@ IF :object_type IN('202') THEN
 			"OWOR"
 		WHERE
 			"Status" in('R','P')
-			AND "CreateDate" >= '2025-01-01'
+			AND "CreateDate" >= '2025-06-01'
 			AND DAYS_BETWEEN("CreateDate", CURRENT_DATE) > 20
 		LIMIT 1)) THEN
 			SELECT
@@ -60,7 +56,7 @@ IF :object_type IN('202') THEN
 				"OWOR"
 			WHERE
 				"Status" in('R','P')
-				AND "CreateDate" >= '2025-01-01'
+				AND "CreateDate" >= '2025-06-01'
 				AND DAYS_BETWEEN("CreateDate", CURRENT_DATE) > 20
 			LIMIT 1;
 			error := '88';
@@ -80,7 +76,7 @@ IF :object_type IN('202') THEN
 			LEFT JOIN "IGE1" saida ON(saida."BaseRef" = to_char(ordem."DocNum"))
 		WHERE
 			"Status" in('R')
-			AND "CreateDate" >= '2025-01-01' 
+			AND "CreateDate" >= '2025-06-01' 
 			AND DAYS_BETWEEN("CreateDate", CURRENT_DATE) > 3
 			AND ordem."DocEntry" <> :list_of_cols_val_tab_del
 			AND ((entrada."DocEntry" IS NOT NULL AND saida."DocEntry" IS NULL) OR (entrada."DocEntry" IS NULL AND saida."DocEntry" IS NOT NULL)))) 
@@ -94,7 +90,7 @@ IF :object_type IN('202') THEN
 				LEFT JOIN "IGE1" saida ON(saida."BaseRef" = to_char(ordem."DocNum"))
 			WHERE
 				"Status" in('R')
-				AND "CreateDate" >= '2025-01-01' 
+				AND "CreateDate" >= '2025-06-01' 
 				AND DAYS_BETWEEN("CreateDate", CURRENT_DATE) > 3
 				AND ((entrada."DocEntry" IS NOT NULL AND saida."DocEntry" IS NULL) OR (entrada."DocEntry" IS NULL AND saida."DocEntry" IS NOT NULL)) LIMIT 1;
 			error := '88';
@@ -117,7 +113,59 @@ IF :object_type IN('202') THEN
 		error := '88';
 		error_message := 'Não é permitido encerrar a ordem de produção sem informar entradas e saidas dos itens.';
 	END if;
+IF EXISTS (
+    SELECT
+        1
+    FROM OWOR O
+    INNER JOIN WOR1 LP
+        ON O."DocEntry" = LP."DocEntry"
+    INNER JOIN OITT R
+        ON O."ItemCode" = R."Code"
+    INNER JOIN ITT1 RL
+        ON RL."Father"   = R."Code"
+       AND LP."ItemCode" = RL."Code"
+    WHERE
+        LP."BaseQty" >= 2 * RL."Quantity"
+      AND O."DocEntry" = :list_of_cols_val_tab_del
+    LIMIT 1
+) THEN
+    SELECT
+        LP."ItemCode"
+    INTO
+        ItemCode
+    FROM OWOR O
+    INNER JOIN WOR1 LP
+        ON O."DocEntry" = LP."DocEntry"
+    INNER JOIN OITT R
+        ON O."ItemCode" = R."Code"
+    INNER JOIN ITT1 RL
+        ON RL."Father"   = R."Code"
+       AND LP."ItemCode" = RL."Code"
+    WHERE
+        LP."BaseQty" >= 2 * RL."Quantity"
+      AND O."DocEntry" = :list_of_cols_val_tab_del
+    LIMIT 1;
 
+    error         := '7';
+    error_message := 'Quantidade base muito diferente da receita no item ' || ItemCode;
+END IF;
+END IF;
+IF :object_type IN('59') THEN
+
+    IF EXISTS (
+        SELECT 1
+        FROM IGN1 o
+        INNER JOIN owor ordem
+            ON ordem."DocNum" = CAST(o."BaseRef" AS INT)
+        WHERE o."DocEntry" = :list_of_cols_val_tab_del 
+          AND o."BaseRef" <> ''
+          AND NOT EXISTS (
+              SELECT 1 FROM IGE1 WHERE "BaseRef" = CAST(ordem."DocNum" AS NVARCHAR)
+          )
+    ) THEN 
+        error := '7';
+        error_message := 'Primeiro deve ser realizado a saida de mecadoria';
+    END IF;
 
 END IF;
 
