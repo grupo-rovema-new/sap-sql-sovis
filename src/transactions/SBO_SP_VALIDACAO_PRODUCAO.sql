8a518fc559a76b0a37ce715c0681a23592c50ead
Francisco Marcello <54758350+franciscoMarcello@users.noreply.github.com>
2025-06-25 08:24:28 -0400

    Adicionando trava para produção (#165)
    * Teste da view
    
    * Metas
    
    * Mudando codigo para criar views da pasta comercial
    
    * Comissão
    
    * Com relatório de comissão
    
    * ajustando comissão
    
    * Arrumando erro do docentry
    
    * Retirando datas dos parametros
    
    * Ajustando relatório de comissão
    
    * Adicionando transaction de bloqueio por filial
    
    * Aplicando trava de utilização
    
    * Ajustando para resolver o problema de mais linhas
    
    * Ajuste para a trava deixar passar notas que não sao de itens
    
    * Correcao endereco
    
    * Funcionalidade: validação da venda futura
    
    * Comentando trava para validacao de venda futura
    
    * Liberacao das travas para permitir venda futura
    
    * Adicionando trava para não permitir quantidade acima que a do contrato
    
    * Removendo comentarios
    
    * Ajustando trava de pis e cofins
    
    * ajustando branch
    
    * Adicionando travas de CTE
    
    * Adicionando trava para utilizacao de doacao e brinde
    
    * Adicionando utilizacao de bonificacao na trava de destinacao
    
    * Comentando trava do CTE e adicionando View para o relatorio de pis e cofins
    
    * Comentando trava
    
    * Ajustando VIEW resumo avulso
    
    * Adicionando trava para nao permitir bug do frete
    
    * Adicionando trava de custo medio
    
    * Ajuste trava de custo meido
    
    * Adicionando excecao na trava de preco base
    
    * Atualizando base pis e cofins
    
    * Adicionando trava de transferencia de mercadoria
    
    * Ajuste de commit
    
    * Adicionando views de GGF
    
    * Ajuste trava de entrega
    
    * Adicionando trava de bonificacao
    
    * Ajustando travas de produção
    
    * Ajustando trava de transferencia
    
    * Adicionando variaveis para debugar trava
    
    * Travas de cte e despesa de impotação
    
    * coloca falacia para evitar trava que não foi validada
    
    * Ajuste
    
    * Adicionando utilização na trava de tranferencia de estoque
    
    * Ajustando commit
    
    * Ajuste trava de devolução
    
    * Adicionando trava de condição de pagamento
    
    * Adicionando trava de quantidade base da receita e processo de encerramento da ordem de producao
    
    * Ajustando data
    
    ---------
    
    Co-authored-by: Andrew Ramires May <andrewc3po@gmail.com>
    Co-authored-by: Andrew Ramires <8061215+malacration@users.noreply.github.com>

--- src/transactions/SBO_SP_VALIDACAO_PRODUCAO.sql
+++ src/transactions/SBO_SP_VALIDACAO_PRODUCAO.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE  PROCEDURE SBO_SP_VALIDACAO_PRODUCAO
+CREATE OR replace PROCEDURE SBO_SP_VALIDACAO_PRODUCAO
 
 (
     in object_type nvarchar(30),                 -- SBO Object Type
@@ -14,21 +14,17 @@ SQL SECURITY INVOKER
 AS
 BEGIN
 	
-	
-	
 IF :object_type IN('202') THEN
-
 	DECLARE idade INT;
     DECLARE temEntrada INT;
 	DECLARE temSaida INT;
-
-	DECLARE docNumMsg INT;
-	   
+	DECLARE ItemCode nvarchar (255);
+	DECLARE docNumMsg INT;  
 /*
  * Anotações
  * Status (L ->  Encerrado, R ->  Liberado, P -> Planejado)
  */
-
+	
 	SELECT DISTINCT 
 		DAYS_BETWEEN("CreateDate", CURRENT_DATE),
 		entrada."DocEntry" AS "temEntrada?",
@@ -117,7 +113,59 @@ IF :object_type IN('202') THEN
 		error := '88';
 		error_message := 'Não é permitido encerrar a ordem de produção sem informar entradas e saidas dos itens.';
 	END if;
+IF EXISTS (
+    SELECT
+        1
+    FROM OWOR O
+    INNER JOIN WOR1 LP
+        ON O."DocEntry" = LP."DocEntry"
+    INNER JOIN OITT R
+        ON O."ItemCode" = R."Code"
+    INNER JOIN ITT1 RL
+        ON RL."Father"   = R."Code"
+       AND LP."ItemCode" = RL."Code"
+    WHERE
+        LP."BaseQty" >= 2 * RL."Quantity"
+      AND O."DocEntry" = :list_of_cols_val_tab_del
+    LIMIT 1
+) THEN
+    SELECT
+        LP."ItemCode"
+    INTO
+        ItemCode
+    FROM OWOR O
+    INNER JOIN WOR1 LP
+        ON O."DocEntry" = LP."DocEntry"
+    INNER JOIN OITT R
+        ON O."ItemCode" = R."Code"
+    INNER JOIN ITT1 RL
+        ON RL."Father"   = R."Code"
+       AND LP."ItemCode" = RL."Code"
+    WHERE
+        LP."BaseQty" >= 2 * RL."Quantity"
+      AND O."DocEntry" = :list_of_cols_val_tab_del
+    LIMIT 1;
 
+    error         := '7';
+    error_message := 'Quantidade base muito diferente da receita no item ' || ItemCode;
+END IF;
+END IF;
+IF :object_type IN('59') THEN
+
+    IF EXISTS (
+        SELECT 1
+        FROM IGN1 o
+        INNER JOIN owor ordem
+            ON ordem."DocNum" = CAST(o."BaseRef" AS INT)
+        WHERE o."DocEntry" = :list_of_cols_val_tab_del 
+          AND o."BaseRef" <> ''
+          AND NOT EXISTS (
+              SELECT 1 FROM IGE1 WHERE "BaseRef" = CAST(ordem."DocNum" AS NVARCHAR)
+          )
+    ) THEN 
+        error := '7';
+        error_message := 'Primeiro deve ser realizado a saida de mecadoria';
+    END IF;
 
 END IF;
 
